a2a340eff4309bbd3733f33ce764a8f0422ef7f7
Naily Khairiya <nailykhairiya@gmail.com>
2023-03-25 12:56:26 +0700

    add download file feature, 404, dir

--- Nomor 6/client/client.py
+++ Nomor 6/client/client.py
@@ -1,6 +1,8 @@
+from inspect import getfile
 import sys
 import socket
 import configparser
+import os
 
 from bs4 import BeautifulSoup
 
@@ -10,6 +12,56 @@ config.read('httpclient.conf')
 # mengambil data port dan host
 port = config['server']['port']
 host = config['server']['host']
+
+
+def getHeader(client):
+    header = b''
+    while True:
+        data = client.recv(1024)
+        header += data
+        if b'\r\n\r\n' in header:
+            break
+    header = header.decode('utf-8')
+    return header
+
+
+def getFile(client, file_name):
+    if not os.path.exists('dataset'):
+        os.mkdir('dataset')
+    path = 'dataset/'+file_name
+    with open(path, 'wb') as f:
+        while True:
+            data = client.recv(1024)
+            if not data:
+                break
+            f.write(data)
+
+
+def getContentType(res):
+    type = res.split('Content-Type: ')[1].split(';')[0]
+    content_type = type.split('/')[0]
+    content_type2 = type.split('/')[1]
+    return content_type, content_type2
+
+
+def getStatusCode(res):
+    status_code = res.split()[1]
+    return status_code
+
+
+def getFileName(req):
+    file_name = req.split('/')[2]
+    return file_name
+
+
+def getBody(client):
+    response = ''
+    while True:
+        received = client.recv(1024)
+        if not received:
+            break
+        response += received.decode('utf-8')
+    return response
 
 
 def main(elements):
@@ -28,22 +80,39 @@ def main(elements):
                               "\r\n").encode('utf-8')
 
             client.send(request_header)
+            header = getHeader(client)
 
-            response = ''
-            while True:
-                received = client.recv(1024)
-                if not received:
-                    break
-                response += received.decode('utf-8')
+            status_code = getStatusCode(header)
+            if(status_code == '200'):
+                content_type, content_type2 = getContentType(header)
 
-            if(e == 'help' or e == '/help'):
-                print(response)
+                if(content_type == 'application'):
+                    file_name = getFileName(e)
+                    getFile(client, file_name)
+                else:
+                    response = getBody(client)
+                    response = header + response
+                    if content_type2 == 'html':
+                        soup = BeautifulSoup(response, 'html.parser')
+                        print(soup.title.string)
+                        print(soup.get_text())
+
+                    else:
+                        print(response)
             else:
-                soup = BeautifulSoup(response, 'html.parser')
-                print(soup.title.string)
-                print(soup.get_text())
-            client.shutdown(socket.SHUT_RDWR)
-            client.close()
+                content_type, content_type2 = getContentType(header)
+                response = getBody(client)
+                response = header + response
+                if content_type2 == 'html':
+                    soup = BeautifulSoup(response, 'html.parser')
+                    print(soup.title.string)
+                    print(soup.get_text())
+
+                else:
+                    print(response)
+
+        client.shutdown(socket.SHUT_RDWR)
+        client.close()
 
     except Exception as msg:
         print(msg)
--- Nomor 6/server/server.py
+++ Nomor 6/server/server.py
@@ -1,10 +1,10 @@
 import configparser
-from importlib.resources import path
 import select
 import socket
 import sys
 import os
 import threading
+
 
 # membaca file konfigurasi
 config = configparser.ConfigParser()
@@ -75,6 +75,17 @@ class Client(threading.Thread):
         self.address = address
         self.size = 1024
 
+    def isFileExist(self, file_name):
+        path = os.getcwdb().decode('utf-8')
+        path = os.path.join(path, 'dataset')
+        dir_list = os.listdir(path)
+        if file_name in dir_list:
+            return True
+        return False
+
+    def sendFile(self, file_path):
+        print('File Send Successfully')
+
     # Proses mengirim dan
     # menerima data
     def run(self):
@@ -93,20 +104,20 @@ class Client(threading.Thread):
 
             # HELP
             if request_file == '/help' or request_file == 'help':
-                response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\n' \
+                response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8\r\n' \
                     + '\r\n\r\n'
 
                 response_data = ('Berikut merupakan command yang dapat digunakan: \n\n' +
-                                 '~ /help atau help : untuk melihat command tersedia\n' +
-                                 '~ / atau /index.html atau index.html : untuk membuka index.html\n' +
-                                 '~ /dataset atau dataset : melihat daftar isi dari dataset\n' +
-                                 '~ /:nama file : untuk mendownload file tertentu\n')
+                                 '~ /help : untuk melihat command tersedia\n' +
+                                 '~ / atau /index.html : untuk membuka index.html\n' +
+                                 '~ /dataset  : melihat daftar isi dari dataset\n' +
+                                 '~ /dataset/:nama file : untuk mendownload file tertentu\n')
 
                 self.client.sendall(response_header.encode(
                     'utf-8') + response_data.encode('utf-8'))
 
             # INDEX HTML
-            elif request_file == 'index.html' or request_file == '/' or request_file == '/index.html':
+            elif request_file == '/' or request_file == '/index.html':
                 f = open('index.html', 'r')
                 response_data = f.read()
                 f.close()
@@ -119,13 +130,45 @@ class Client(threading.Thread):
                     'utf-8') + response_data.encode('utf-8'))
 
             # LIST DIRECTORY / DATASET
-            elif request_file == '/dataset' or request_file == 'dataset':
+            elif request_file == '/dataset':
                 path = os.getcwdb().decode('utf-8')
                 path = os.path.join(path, 'dataset')
                 dir_list = os.listdir(path)
-                print(dir_list)
+                dir_list = '\n'.join(dir_list)
+                response_header = 'HTTP/1.1 200 OK\r\nContent-Type: text/plain; charset=UTF-8' \
+                    + '\r\n\r\n'
+
+                self.client.sendall(response_header.encode(
+                    'utf-8') + dir_list.encode('utf-8'))
+
+            elif request_file.split('/')[1] == 'dataset':
+                file_name = request_file.split('/')[2]
+                if self.isFileExist(file_name):
+                    # Download file
+                    file_path = 'dataset/' + file_name
+                    content_length = os.path.getsize(file_path)
+                    response_header = 'HTTP/1.1 200 OK\r\nContent-Type: application/zip; charset=UTF-8\r\nContent-Length:' \
+                        + str(content_length) + '\r\n\r\n'
+                    self.client.send(response_header.encode('utf-8'))
+                    with open(file_path, 'rb') as f:
+                        data = f.read(1024)
+                        while data:
+                            self.client.send(data)
+                            data = f.read(1024)
+                else:
+                    f = open('404.html', 'r')
+                    response_data = f.read()
+                    f.close()
+
+                    content_length = len(response_data)
+                    response_header = 'HTTP/1.1 404 Not Found\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length:' \
+                        + str(content_length) + '\r\n\r\n'
+
+                    self.client.sendall(response_header.encode(
+                        'utf-8') + response_data.encode('utf-8'))
 
             else:
+
                 f = open('404.html', 'r')
                 response_data = f.read()
                 f.close()
